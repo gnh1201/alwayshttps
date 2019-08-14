3f24fc9982ef52d6b2e267e1f8c7f41ae8e6ca79
Namhyeon, Go <gnh1201@gmail.com>
2019-08-14 16:51:37 +0900

    Update alwayshttps.js

--- alwayshttps.js
+++ alwayshttps.js
@@ -2,112 +2,125 @@
  * @file alwayshttps.js
  * @license http://www.opensource.org/licenses/mit-license.html  MIT License
  * @author Go Namhyeon <gnh1201@gmail.com>
- * @date: 2018-02-17
+ * @date 2018-02-17
+ * @updated 2019-08-14
  */
 
 function alwayshttps_replace_from(uri) {
-	return uri.replace("http://", "https://", uri);
+    return uri.replace("http://", "https://", uri);
 }
 
 function alwayshttps_decision(a, b) {
-	return ('https:' == window.location.protocol) ? a : b;
+    return ('https:' == window.location.protocol) ? a : b;
 }
 
 function alwayshttps_redirect() {
-	if(!alwayshttps_decision(true, false)) {
-		location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-	}
+    if(!alwayshttps_decision(true, false)) {
+        location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+    }
 }
 
 function alwayshttps_dom() {
-	var secureTagNames = [
-		'meta', 'link', 'script', 'embed', 'iframe',
-		'img', 'input', 'source', 'audio', 'video',
-		'track', 'a', 'form', 'object', 'area',
-		'applet', 'frame', 'param'
-	];
+    var secureTagNames = [
+        'meta', 'link', 'script', 'embed', 'iframe',
+        'img', 'input', 'source', 'audio', 'video',
+        'track', 'a', 'form', 'object', 'area',
+        'applet', 'frame', 'param'
+    ];
 
-	for(var i in secureTagNames) {
-		var doms = document.getElementsByTagName(secureTagNames[i]);
-		for(var k in doms) {
-			var domUri = "";
-			var domAttrName = "";
-			var secureDomUri = "";
+    for(var i in secureTagNames) {
+        var doms = document.getElementsByTagName(secureTagNames[i]);
+        for(var k in doms) {
+            var domUri = "";
+            var domAttrName = "";
+            var secureDomUri = "";
 
-			switch(secureTagNames[i]) {
-				// meta->content
-				case "meta":
-					domAttrName = "content";
-					break;
+            switch(secureTagNames[i]) {
+                // meta->content
+                case "meta":
+                    domAttrName = "content";
+                    break;
 
-				// link->href, a->href, area->href
-				case "link":
-				case "a":
-				case "area":
-					domAttrName = "href";
-					break;
+                // link->href, a->href, area->href
+                case "link":
+                case "a":
+                case "area":
+                    domAttrName = "href";
+                    break;
 
-				// form->action
-				case "form":
-					domAttrName = "action";
-					break;
+                // form->action
+                case "form":
+                    domAttrName = "action";
+                    break;
 
-				// script->src, embed->src, iframe->src, img->src, input->src
-				// source->src, audio->src, video->src, track->src, frame->src
-				case "script":
-				case "embed":
-				case "iframe":
-				case "img":
-				case "input":
-				case "source":
-				case "audio":
-				case "video":
-				case "track":
-				case "frame":
-					domAttrName = "src";
-					break;
+                // script->src, embed->src, iframe->src, img->src, input->src
+                // source->src, audio->src, video->src, track->src, frame->src
+                case "script":
+                case "embed":
+                case "iframe":
+                case "img":
+                case "input":
+                case "source":
+                case "audio":
+                case "video":
+                case "track":
+                case "frame":
+                    domAttrName = "src";
+                    break;
 
-				// object->data
-				case "object":
-					domAttrName = "data";
-					break;
+                // object->data
+                case "object":
+                    domAttrName = "data";
+                    break;
 
-				// applet->codebase
-				case "applet":
-					domAttrName = "codebase";
-					break;
+                // applet->codebase
+                case "applet":
+                    domAttrName = "codebase";
+                    break;
 
-				// object->param->value
-				case "param":
-					domAttrName = "value";
-					break;
+                // object->param->value
+                case "param":
+                    domAttrName = "value";
+                    break;
 
-				default:
-					domAttrName = "";
-			}
+                default:
+                    domAttrName = "";
+            }
 
-			// get uri of dom element
-			if(doms[k] != null && typeof(doms[k].getAttribute) !== "undefined") {
-				domUri = doms[k].getAttribute(domAttrName);
-			}
+            // get uri of dom element
+            if(doms[k] != null && typeof(doms[k].getAttribute) !== "undefined") {
+                domUri = doms[k].getAttribute(domAttrName);
+            }
 
-			// detect http, and replace to https
-			if(domUri != null && domUri.indexOf("http://") > -1) {
-				secureDomUri = alwayshttps_replace_from(domUri);
-				doms[k].setAttribute(domAttrName, secureDomUri);
-			}
-		}
-	}
+            // detect http, and replace to https
+            if(domUri != null && domUri.indexOf("http://") > -1) {
+                secureDomUri = alwayshttps_replace_from(domUri);
+                doms[k].setAttribute(domAttrName, secureDomUri);
+            }
+        }
+    }
 }
 
 function alwayshttps_load() {
-	alwayshttps_redirect();
-	alwayshttps_dom();
+    alwayshttps_redirect();
+    alwayshttps_dom();
 }
 
-// load alwayshttps when ready document
-if(typeof(jQuery) !== "undefined") {
-	jQuery(document).ready(function() { alwayshttps_load(); });
-} else {
-	window.onload = function() { alwayshttps_load(); };
+function alwayshttps_polling() {
+    alwayshttps_dom();
+    setTimeout(alwayshttps_polling, 300);
 }
+
+function alwayshttps_main() {
+    // load AlwaysHTTP when ready document
+    if(typeof(jQuery) !== "undefined") {
+        jQuery(document).ready(function() { alwayshttps_load(); });
+    } else {
+        window.onload = function() { alwayshttps_load(); };
+    }
+
+    // check AlwaysHTTP integrity each 0.3 seconds
+    alwayshttps_polling();
+}
+
+alwayshttps_main();
